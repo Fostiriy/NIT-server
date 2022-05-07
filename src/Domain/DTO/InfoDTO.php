9499a102b4299452c83cc5eed550d390be5bb995
Fostiriy <burakov.aa@students.dvfu.ru>
2022-05-07 15:41:54 +1000

    Добавить composer и twig

---
+++ src/Domain/DTO/InfoDTO.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Domain\DTO;
+
+class InfoDTO {
+    private $name;
+    private $surname;
+    private $age;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * @param mixed $surname
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+}
