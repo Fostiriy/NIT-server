28e24185a90ecc10bae0e9c1ca37630df373bcfd
Fostiriy <burakov.aa@students.dvfu.ru>
2022-05-20 18:58:52 +1000

    Добавить composer, twig, monolog в проект

---
+++ src/Application/Web/AboutMyselfController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Application\Web;
+
+use App\Domain\DTO\InfoDTO;
+use Twig\Environment;
+
+class AboutMyselfController
+{
+    private $twig;
+
+    /**
+     * @return Environment
+     */
+    public function getTwig()
+    {
+        return $this->twig;
+    }
+
+    /**
+     * @param Environment $twig
+     */
+    public function setTwig($twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function __invoke()
+    {
+        $info = new InfoDTO();
+        $info->setName("Alexey");
+        $info->setSurname("Burakov");
+        $info->setAge(20);
+
+        $contacts = [
+            "steam" => "fostiriy",
+            "vk" => "alexeybur",
+            "phone" => "89619502118",
+        ];
+
+        return $this->twig->render("web/about-myself.html.twig", [
+            "title" => "Page",
+            "info" => $info,
+            "contacts" => $contacts,
+        ]);
+    }
+}
