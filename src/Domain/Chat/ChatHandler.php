e24197c01823e6a97c8e9b0e077d67bfd2ed6cd4
Fostiriy <burakov.aa@students.dvfu.ru>
2022-06-13 20:30:44 +1000

    Использовать save() из MessageRepository.php в чате

--- src/Domain/Chat/ChatHandler.php
+++ src/Domain/Chat/ChatHandler.php
@@ -2,6 +2,7 @@
 
 namespace Domain\Chat;
 
+use Domain\Entity\Message;
 use Domain\Entity\User;
 use Domain\Repository\MessageRepository;
 use Monolog\Handler\StreamHandler;
@@ -36,55 +37,42 @@ class ChatHandler
 
     public function print_messages($user_name)
     {
-        try {
-            $messages = $this->messageRepository->getAll();
-            $author_id = 0;
-            if ($user_name === "admin") {
-                $users = $this->user->getAll();
-                echo '<pre>';
-                print_r($users);
-                echo '</pre>';
-            } else {
-                $this->user->setUserName($user_name);
-                $author_id = $this->user->findID();
-            }
+        $messages = $this->messageRepository->getAll();
+        $author_id = 0;
+        if ($user_name === "admin") {
+            $users = $this->user->getAll();
+            echo '<pre>';
+            print_r($users);
+            echo '</pre>';
+        } else {
+            $this->user->setUserName($user_name);
+            $author_id = $this->user->findID();
+        }
 
-            foreach ($messages as $message) {
-                if ($author_id == 0 || $author_id == $message->getAuthorId()) {
-                    $this->twig->display("web/message.html.twig", [
-                        "message" => [
-                            "date" => $message->getMessageDate(),
-                            "user" => $this->user->getByID($message->getAuthorId())->getUserName(),
-                            "message" => $message->getMessageText(),
-                        ],
-                    ]);
-                }
+        foreach ($messages as $message) {
+            if ($author_id == 0 || $author_id == $message->getAuthorId()) {
+                $this->twig->display("web/message.html.twig", [
+                    "message" => [
+                        "date" => $message->getMessageDate(),
+                        "user" => $this->user->getByID($message->getAuthorId())->getUserName(),
+                        "message" => $message->getMessageText(),
+                    ],
+                ]);
             }
-        } catch (PDOException $e) {
-            echo "Error!: " . $e->getMessage() . "<br/>";
         }
     }
 
-    public function add_message($user)
+    public function add_message($user_name)
     {
-        $message = empty($_GET["message"]) ? "" : $_GET["message"];
+        $messageText = empty($_GET["message"]) ? "" : $_GET["message"];
 
         // adding message
-        if (isset($message) && $message !== "") {
-            try {
-                $query = $this->DBH->prepare("SELECT user_id FROM user WHERE user_name = ?");
-                $query->execute([$user]);
-                $author_id = $query->fetchColumn();
-                $query = $this->DBH->prepare("INSERT INTO chat(message_date, author_id, message_text) VALUE (?, ?, ?)");
-                $query->execute([
-                    date('Y-m-d H:i:s', time()), $author_id, $message,
-                ]);
-            } catch (PDOException $e) {
-                echo "Error!: " . $e->getMessage() . "<br/>";
-            }
+        if (isset($messageText) && $messageText !== "") {
+            $this->user->setUserName($user_name);
+            $this->messageRepository->save(new Message($this->user->findID(), $messageText));
 
             $this->log->pushHandler($this->chat_handler);
-            $this->log->info("New message", ["username" => $user]);
+            $this->log->info("New message", ["username" => $user_name]);
         }
     }
 
